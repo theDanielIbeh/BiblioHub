4a7d66affb8c8d2df7658452c5c89d9b4b945c28
theDanielIbeh <dibeh1000@gmail.com>
2024-02-22 15:07:54 +0000

    Extras

--- app/src/main/java/com/example/bibliohub/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bibliohub.utils
-
-class Constants private constructor() {
-    companion object {
-        const val APP_IDENTIFIER = "BiblioHub"
-        const val USER = "user"
-        const val IS_LOGGED_IN = "is_logged_in"
-    }
-}
