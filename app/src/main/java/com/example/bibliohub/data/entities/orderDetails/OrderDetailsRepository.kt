ee9264bb2ba036c757a224d969ff5404a807ac51
adesayolawson <adesayo.lawson@babbangona.com>
2024-02-29 09:14:42 +0100

    Added functions to fetch and update OrderDetails

--- app/src/main/java/com/example/bibliohub/data/entities/orderDetails/OrderDetailsRepository.kt
+++ app/src/main/java/com/example/bibliohub/data/entities/orderDetails/OrderDetailsRepository.kt
@@ -6,6 +6,15 @@ interface OrderDetailsRepository {
     suspend fun insert(orderDetails: OrderDetails)
 
     suspend fun getOrderDetailsByOrderId(orderId: Int): Flow<List<OrderDetails>?>
+
+    fun getOpenOrderDetails(orderID: Int): Flow<List<OrderDetails>>
+
+    suspend fun insertOrUpdate(orderDetails: OrderDetails)
+
+    suspend fun deleteOrderDetails(orderID: Int,productID:Int)
+
+
+
 }
 
 class OfflineOrderDetailsRepository(
@@ -17,4 +26,16 @@ class OfflineOrderDetailsRepository(
 
     override suspend fun getOrderDetailsByOrderId(orderId: Int): Flow<List<OrderDetails>?> =
         orderDetailsDao.getOrderDetailsByOrderId(orderId)
+
+    override fun getOpenOrderDetails(orderID: Int): Flow<List<OrderDetails>> {
+        return orderDetailsDao.getOpenOrderDetails(orderID)
+    }
+
+    override suspend fun insertOrUpdate(orderDetails: OrderDetails) {
+        orderDetailsDao.insertOrUpdate(orderDetails)
+    }
+
+    override suspend fun deleteOrderDetails(orderID: Int, productID: Int) {
+        orderDetailsDao.deleteOrderDetailsByOrderAndProductID(orderID, productID)
+    }
 }
